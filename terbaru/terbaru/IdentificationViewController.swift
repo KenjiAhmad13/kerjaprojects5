116191f96124273b73a6e14c4b45d4e0fb793724
KenjiAhmad13 <151930567+KenjiAhmad13@users.noreply.github.com>
2023-11-24 19:22:39 +0700

    Add files via upload

---
+++ terbaru/terbaru/IdentificationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  IdentificationViewController.swift
+//  terbaru
+//
+//  Created by Keishits on 24/11/23.
+//
+
+import UIKit
+
+class IdentificationViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func letsgoButton(_ sender: UIButton) {
+      
+    }
+    
+}
